0310a3a0192cb46476a694aee96db05dadd08e19
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-16 20:33:48 -0300

    Add categoryDetails view draft

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,7 @@
 const Category = require("../models/category.js");
+const Item = require("../models/item.js");
+const async = require("async");
+const mongoose = require("mongoose");
 
 exports.categoryList = (req, res, next) => {
     Category.find({})
@@ -14,7 +17,30 @@ exports.categoryList = (req, res, next) => {
 }
 
 exports.categoryDetail = (req, res, next) => {
-    res.send(`Category detail: ${req.params.id}`);
+    async.parallel(
+        {
+            category(callback) {
+                Category.findById(req.params.id).exec(callback);
+            },
+            items(callback) {
+                Item.find({category: mongoose.Types.ObjectId(req.params.id)}).exec(callback);
+            }
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+            if(results.category == null){
+                const err = new Error("Category not found");
+                err.status = 404;
+                return next(err);
+            }
+            res.render("categoryDetail", {
+                category: results.category,
+                items: results.items
+            });
+        }
+    );
 }
 
 exports.categoryCreateGet = (req, res, next) => {
