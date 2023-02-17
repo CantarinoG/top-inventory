4d3448e4464bf104a04570efb4a75eabeeb0b259
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-17 10:30:05 -0300

    Add itemCreate POST function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -47,9 +47,58 @@ exports.itemCreateGet = (req, res, next) => {
     );
 }
 
-exports.itemCreatePost = (req, res, next) => {
-    res.send("Item create POST");
-}
+exports.itemCreatePost = [
+    body("name", "Name must not be empty.")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+    body("description", "Description must not be empty.")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+    body("price", "Price must be a number.")
+    .isNumeric().escape(),
+    body("price", "Price must not be empty.")
+    .notEmpty().escape(),
+    body("inStock", "In Stock must not be empty.")
+    .notEmpty().escape(),
+    body("inStock", "In Stock must be a number.")
+    .isNumeric().escape(),
+    body("category", "A category must be selected.")
+    .notEmpty().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        const item = new Item({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+            inStock: req.body.inStock
+        });
+        if (!errors.isEmpty()) {
+            Category.find({}).sort({name: 1}).exec(
+                function (err, listCategories) {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.render("itemCreate", {
+                        listCategories: listCategories,
+                        item: item,
+                        errors: errors.array()
+                    });
+                }
+            );
+        return;
+        }
+        item.save((err) => {
+            if(err) {
+                return next(err);
+            }
+            res.redirect(item.url);
+        })
+
+    }
+];
 
 exports.itemDeleteGet = (req, res, next) => {
     res.send("Item delete GET");
